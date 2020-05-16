406d50edefcfdd4bf2ded0fa7539eb6782487c2a
olufemi abayomi <abayomiolufemi64@yahoo.com>
2020-05-16 12:49:25 +0100

    intial commit

---
+++ Domain/Values.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Domain
+{
+    public class Values
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ Persistence/DataContext.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace Persistence
+{
+    public class DataContext: DbContext
+    {
+        public DataContext(DbContextOptions options): base(options)
+        {
+            
+        }
+
+        public DbSet<Values> Values { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<Values>()
+                .HasData(
+                    new Values {Id = 1, Name = "Value 101"},
+                    new Values {Id = 2, Name = "Value 102"},
+                    new Values {Id = 3, Name = "Value 103"}
+                );
+        }
+    }
+}
